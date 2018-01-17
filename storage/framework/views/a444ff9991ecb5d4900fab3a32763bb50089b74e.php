f57bf93d4e1713dcde2d9e57f18ea1bb68d151e7
Sunil Raj <sunil.raj358@gmail.com>
2018-01-17 15:41:03 +0800

    navbar and prefooter update

--- storage/framework/views/a444ff9991ecb5d4900fab3a32763bb50089b74e.php
+++ storage/framework/views/a444ff9991ecb5d4900fab3a32763bb50089b74e.php
@@ -7,7 +7,22 @@
 	<div class = "col-lg-6 col-lg-offset-2 nav-links">
 		<ul>
 			<li class = "link">Home</li>
-			<li class = "link">Opportunities</li>
+			<div class="dropdown">
+				<button class="dropbtn">Opportunities</button>
+				<div class="dropdown-content">
+					<div class = "col-lg-6">
+						<p class = "dropdown-content-header">United States</p>
+						<a href="#">Work & Travel</a>
+						<a href="#">Internship</a>
+						<a href="#">Au Pair</a>
+					</div>
+					<div class = "col-lg-6">
+						<p class = "dropdown-content-header">Australia</p>
+						<a href="#">Internship</a>
+						<a href="#">Skilled Migration</a>
+					</div>
+				</div>
+			</div>
 			<li class = "link">About</li>
 			<li class = "link">Contact</li>
 			<div class="dropdown">
@@ -20,7 +35,7 @@
 				</div>
 			</div>
 			<div class = "applynow-btn">
-				<a href = "" class = "btn applynow-btn">Apply Now</a></li>
+				<a href = "" class = "btn applynow-btn">Apply Now</a>
 			</div>
 		</ul>
 	</div>
