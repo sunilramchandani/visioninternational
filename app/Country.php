e2dde959ea8852a9dbcd03e5393d8e141e79fc1e
ndar0430 <nikkoremulla010498@gmail.com>
2018-01-17 17:36:56 +0800

    country

---
+++ app/Country.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $table = 'contact_us'; 
+    protected $primaryKey = 'contact-us_id';
+}
