00ca38f9af94da72424542749a7dd36bc2304552
Sunil Raj <sunil.raj358@gmail.com>
2018-01-17 12:20:17 +0800

    prefooter done

--- storage/framework/views/7e1f1f655f7efbb2cd6e309cb8928f95ea32f1b3.php
+++ storage/framework/views/7e1f1f655f7efbb2cd6e309cb8928f95ea32f1b3.php
@@ -1,13 +1,15 @@
 <link rel="stylesheet" href="<?php echo e(asset('css/prefooter.css')); ?>">
 
 <div class = "prefooter">
-    <div class = "row top-prefooter-content text-center">
-        <p class = "banner-paragraph">Start an amazing future with us! </p> 
-        <a class = "btn applynow-button" href = "#"> Apply Now</a>
+    <div class = "row bg-color">
+        <div class = "top-prefooter-content text-center">
+            <p class = "banner-paragraph">Start an amazing future with us! </p> 
+            <a class = "btn applynow-button" href = "#"> Apply Now</a>
+        </div>
     </div>
     <div class = "row bottom-prefooter-content text-center">
         <p class = "subscribe-paragraph">Get the latest updates and news: </p>
-        <input type = "email" class = "input-email-subscribe" name = "subscribe-email" placeholder="Enter your email address">
-        <a class = "btn subscribe-button"><i class="fa fa-1x fa-angle-right" aria-hidden="true"></i></a>
+        <input type = "email" class = "input-email-subscribe text-center" name = "subscribe-email" placeholder="Enter your email address">
+        <a class = "btn subscribe-button"><i class="fa fa-2x fa-angle-right" aria-hidden="true"></i></a>
     </div>
 </div>
