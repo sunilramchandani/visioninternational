1ff97b5162fa1cf35ce89fce810c1e629f5953f5
Sunil Raj <sunil.raj358@gmail.com>
2018-01-19 11:27:13 +0800

    application front done

--- storage/framework/views/7a0cb7166264173d95663a506654e3dbd6497567.php
+++ storage/framework/views/7a0cb7166264173d95663a506654e3dbd6497567.php
@@ -21,85 +21,202 @@
         </div>
         <div class = "col-lg-4 col-lg-offset-1">
         	<div class = "row form-group">
-                <select class = "form-control" name="program" id="">
-                    <option value="" disabled selected>Select</option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
+        		<div class = "col-lg-4">
+        			<label for = "program" class = "labels">Program</label>
+        		</div>
+        		<div class = "col-lg-8">
+	                <select class = "form-control" name="program" id="">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+            	</div>
             </div>
             <div class = "row form-group">
-                <select class = "form-control" name="country" id="">
-                    <option value="" disabled selected>Select</option>
-                    <option value="PS">Palestinian Territory, Occupied</option>
-                    <option value="PA">Panama</option>
-                    <option value="PG">Papua New Guinea</option>
-                    <option value="PY">Paraguay</option>
-                    <option value="PE">Peru</option>
-                    <option value="PH">Philippines</option>
-                    <option value="PN">Pitcairn</option>
-                    <option value="PL">Poland</option>
-                    <option value="PT">Portugal</option>
-                    <option value="PR">Puerto Rico</option>
-                    <option value="QA">Qatar</option>
-                </select>
+            	<div class = "col-lg-4">
+        			<label for = "country" class = "labels">Country</label>
+        		</div>
+        		<div class = "col-lg-8">
+	                <select class = "form-control" name="country" id="">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value="PS">Palestinian Territory, Occupied</option>
+	                    <option value="PA">Panama</option>
+	                    <option value="PG">Papua New Guinea</option>
+	                    <option value="PY">Paraguay</option>
+	                    <option value="PE">Peru</option>
+	                    <option value="PH">Philippines</option>
+	                    <option value="PN">Pitcairn</option>
+	                    <option value="PL">Poland</option>
+	                    <option value="PT">Portugal</option>
+	                    <option value="PR">Puerto Rico</option>
+	                    <option value="QA">Qatar</option>
+	                </select>
+	           </div>
             </div>
             <div class = "row form-group">
-                <select class = "form-control" name="location" id="">
-                    <option value="" disabled selected>Select</option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
+            	<div class = "col-lg-4">
+        			<label for = "location" class = "labels">Location</label>
+        		</div>
+        		<div class = "col-lg-8">
+	                <select class = "form-control" name="location" id="">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+	            </div>
             </div>
             <div class = "row form-group">
-                <input type = "text" class = "form-control"  name="last_name" id="">
+            	<div class = "col-lg-4">
+        			<label for = "last_name" class = "labels">Last Name</label>
+        		</div>
+        		<div class = "col-lg-8">
+                	<input type = "text" class = "form-control"  name="last_name" id="">
+                </div>
             </div>
             <div class = "row form-group">
-                <input type = "text" class = "form-control"  name="first_name" id="">
+            	<div class = "col-lg-4">
+        			<label for = "first_name" class = "labels">First Name</label>
+        		</div>
+        		<div class = "col-lg-8">
+                	<input type = "text" class = "form-control"  name="first_name" id="">
+                </div>
             </div>
             <div class = "row form-group">
-                <input type = "email" class = "form-control"  name="email" id="">
+            	<div class = "col-lg-4">
+        			<label for = "email" class = "labels">Email</label>
+        		</div>
+        		<div class = "col-lg-8">
+                	<input type = "email" class = "form-control"  name="email" id="">
+                </div>
             </div>
             <div class = "row form-group">
-                <input type = "tel" class = "form-control"  name="contact_no" id="" >
+            	<div class = "col-lg-4">
+        			<label for = "contact_no" class = "labels">Contact Number</label>
+        		</div>
+        		<div class = "col-lg-8">
+                	<input type = "tel" class = "form-control"  name="contact_no" id="" >
+                </div>
             </div>
             <div class = "row form-group">
-                <input type = "date" class = "form-control"  name="bdate" id="" >
+            	<div class = "col-lg-4">
+        			<label for = "bdate" class = "labels">Birthdate</label>
+        		</div>
+        		<div class = "col-lg-8">
+                	<input type = "date" class = "form-control"  name="bdate" id="" >
+                </div>
             </div>
             <div class = "row form-group">
-                <select class = "form-control" name="gender" id="">
-                    <option value="" disabled selected>Select</option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
+            	<div class = "col-lg-4">
+        			<label for = "gender" class = "labels">Gender</label>
+        		</div>
+        		<div class = "col-lg-8">
+	                <select class = "form-control" name="gender" id="">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+	            </div>
             </div>
             <div class = "row form-group">
-                <select class = "form-control" name="current_city" id="">
-                    <option value="" disabled selected>Select</option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
+            	<div class = "col-lg-4">
+        			<label for = "current_city" class = "labels">Current City</label>
+        		</div>
+        		<div class = "col-lg-8">
+	                <select class = "form-control" name="current_city" id="">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+	            </div>
             </div>
         </div>
-        <div class = "col-lg-5 col-lg-offset-1">
+        <div class = "col-lg-4 col-lg-offset-1">
             <div class = "row form-group">
-                <select class = "form-control" name="general_inquiries">
-                    <option value="" disabled selected>General Inquiries</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </select>
+        		<div class = "col-lg-5">
+        			<label for = "school" class = "labels">University/School</label>
+        		</div>
+        		<div class = "col-lg-7">
+	                <select class = "form-control" name="school" id="school">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+            	</div>
             </div>
             <div class = "row form-group">
-                <textarea name="message" class = "form-control textarea-input" placeholder="Message" rows="8" cols="50" name="message" id=""></textarea>
+        		<div class = "col-lg-5">
+        			<label for = "degree" class = "labels">Degree</label>
+        		</div>
+        		<div class = "col-lg-7">
+	                <select class = "form-control" name="degree" id="degree">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+            	</div>
+            </div>
+            <div class = "row form-group">
+        		<div class = "col-lg-5">
+        			<label for = "major" class = "labels">Major</label>
+        		</div>
+        		<div class = "col-lg-7">
+	                <select class = "form-control" name="major" id="major">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+            	</div>
+            </div>
+            <div class = "row form-group">
+            	<div class = "col-lg-5">
+        			<label for = "grad" class = "labels">Graduation Date</label>
+        		</div>
+        		<div class = "col-lg-7">
+                	<input type = "date" class = "form-control"  name="grad" id="grad" >
+                </div>
+            </div>
+            <div class = "row form-group">
+            	<div class = "col-lg-5">
+        			<label for = "start" class = "labels">Preferred Start Date</label>
+        		</div>
+        		<div class = "col-lg-7">
+                	<input type = "date" class = "form-control"  name="start" id="start" >
+                </div>
+            </div>
+            <div class = "row form-group">
+            	<div class = "col-lg-5">
+        			<label for = "resume" class = "labels">Upload Resume</label>
+        		</div>
+        		<div class = "col-lg-7">
+                	<input type = "file" class = "form-control"  name="resume" id="resume" >
+                </div>
+            </div>
+            <div class = "row form-group">
+        		<div class = "col-lg-5">
+        			<label for = "learn" class = "labels">How did you learned about V.I.P.?</label>
+        		</div>
+        		<div class = "col-lg-7">
+	                <select class = "form-control" name="learn" id="learn">
+	                    <option value="" disabled selected>Select</option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                    <option value=""></option>
+	                </select>
+            	</div>
+            </div>
+            <div class = "row form-group">
+                <textarea name="message" class = "form-control textarea-input" placeholder="Message" rows="5" cols="50" name="message" id=""></textarea>
             </div>
         </div>
-        <div class = "col-lg-3 col-lg-offset-9 text-center">
-            <button class = "submit btn"><span>Send Message</span></button>
+        <div class = "col-lg-3 col-lg-offset-8 text-center">
+            <button class = "submit btn"><span>Submit Form</span></button>
         </div>
     </div>  
 </div>
