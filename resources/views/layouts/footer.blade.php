18316c0dac200530b9909eef37ae795945563ea1
theseus <ramchandanisunilraj@gmail.com>
2018-02-17 16:34:07 +0800

    footer and application responsive

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 
 <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
     <div class = "row top-footer-content">
-        <div class = "col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-8 col-sm-offset-4 col-xs-8 col-xs-offset-4 vip-logo">
+        <div class = "col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-8 col-sm-offset-4 col-xs-8 col-xs-offset-3 vip-logo">
             <img src = "{{ URL::asset('image/footer-logo.png')}}" class = "img img-responsive footer-logo">
         </div>
         <div class = "col-lg-2 col-md-2 col-sm-3 col-xs-3 company">
@@ -42,10 +42,10 @@
         <hr>
     </div>
     <div class = "col-lg-12 col-md-12 col-sm-12 bottom-footer-content">
-        <div class = "col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3 col-sm-offset-1 col-xs-3 col-xs-offset-1">
+        <div class = "col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3 col-sm-offset-1 col-xs-4">
             <p>Crafted by Sky Rocket PH</p>
         </div>
-        <div class = "col-lg-6 col-lg-offset-2 col-md-6 col-md-offset-2 col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-2">
+        <div class = "col-lg-6 col-lg-offset-2 col-md-6 col-md-offset-2 col-sm-6 col-sm-offset-2 col-xs-8">
             <p>&copy; 2017, Vision International Philippines All rights reserved.   Privacy Policy. Terms of Service </p>
         </div>
     </div>
